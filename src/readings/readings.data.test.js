923a6b9e44a898247ca1046b9441037f7afa554c
Kamil Posiadała <3dcreator.pl@gmail.com>
2023-06-11 17:07:09 +0000

    +test for data generation

--- src/readings/readings.data.test.js
+++ src/readings/readings.data.test.js
@@ -5,4 +5,13 @@ describe("generate data", () => {
     it("should generate readings for one meter", () => {
         expect(readingsData[meters.METER0].length).toBeGreaterThan(0);
     });
+
+    it("should generate readings for each meter", () => {
+        expect(readingsData[meters.METER0].length).toBeGreaterThan(0);
+        expect(readingsData[meters.METER1].length).toBeGreaterThan(0);
+        expect(readingsData[meters.METER2].length).toBeGreaterThan(0);
+        expect(readingsData[meters.METER3].length).toBeGreaterThan(0);
+        expect(readingsData[meters.METER4].length).toBeGreaterThan(0);
+        expect(Object.keys(readingsData).length).toBe(5);
+    });
 });
